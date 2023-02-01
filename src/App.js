7e21181774ed3ead366a67493f7e59285c3c561e
bahaa1985 <bahaa_salaheldin@live.com>
2023-02-01 20:50:58 +0200

    standings4

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import './App.css';
 import Standings from './presentation/Standings'
 
 function App() {
-  //let standingsInfo={leagueId:leagueId,seasonId:seasonId}
+
   let [leagueId,setLeagueId]=useState(0)
   let [seasonId,setSeasonId]=useState(0)
+  const year=new Date().getFullYear()
   return (
     <div className="App">
      <div>
-      <button>UFL</button>
-      <button  onClick={()=>{setLeagueId(39);setSeasonId(2022)}}>EPL</button>
-      <button>La Liga</button>
-      <button>Le Calcio</button>
-      <button>Bundesliga</button>
-      <button>Liga Un</button>
+      <button onClick={()=>{setLeagueId(2);setSeasonId(year)}}>UFL</button>
+      <button onClick={()=>{setLeagueId(39);setSeasonId(year)}}>EPL</button>
+      <button onClick={()=>{setLeagueId(140);setSeasonId(year)}}>La Liga</button>
+      <button onClick={()=>{setLeagueId(135);setSeasonId(year)}}>Le Calcio</button>
+      <button onClick={()=>{setLeagueId(78);setSeasonId(year)}}>Bundesliga</button>
+      <button onClick={()=>{setLeagueId(61);setSeasonId(year)}}>Liga Un</button>
      </div>
     <Standings leagueId={leagueId} seasonId={seasonId}/>
     </div>
--- src/presentation/Standings.js
+++ src/presentation/Standings.js
@@ -7,22 +7,22 @@ function Standings(props){
 const [standings,setStandings]=useState([])
 
     useEffect(()=>{
-    
-        setStandings(getStandings(props.leagueId,props.seasonId).then((response)=>{
-           return response.data.response[0].league.standings[0]                        
-        // console.log(response.data.response[0].league.standings[0])
-        })
-        // .then(()=>{
-        //     // console.log(standings)
-        // })
-        )
+        if(props.leagueId>0){
+            getStandings(props.leagueId,props.seasonId).then((response)=>{
+                setStandings( response.data.response[0].league.standings[0] )                                          
+            })       
+            
+        }
+        
     },[props])
 
+   
     return(
         <div>
             <table className='table table-hover'>
                 <thead>
                     <tr>
+                        <td>Rank</td>
                         <td>Team</td>
                         <td>Games</td>
                         <td>Win</td>
@@ -31,22 +31,28 @@ const [standings,setStandings]=useState([])
                         <td>GF</td>
                         <td>GA</td>
                         <td>Points</td>
+                      
                     </tr>
                 </thead>
                 <tbody>
-                    {standings.map((d)=>{
-                        return(
-                            <tr>
-                                <td>{d.team.name}</td>
-                                <td>{d.all.played}</td>
-                                <td>{d.all.win}</td>
-                                <td>{d.all.draw}</td>
-                                <td>{d.all.lose}</td>
-                                <td>{d.all.goals.for}</td>
-                                <td>{d.all.goals.against}</td>
-                                <td>{d.points}</td>
-                            </tr>
-                        )
+                    {standings.map((d)=>{                                              
+                       
+                            return(
+                                <tr key={d.team.id}>
+                                    <td>{d.rank}</td>
+                                    <td>{d.team.name}</td>
+                                    <td>{d.all.played}</td>
+                                    <td>{d.all.win}</td>
+                                    <td>{d.all.draw}</td>
+                                    <td>{d.all.lose}</td>
+                                    <td>{d.all.goals.for}</td>
+                                    <td>{d.all.goals.against}</td>
+                                    <td>{d.points}</td>
+                                </tr>
+                                
+                            )
+                        // }
+                        
                     })}
                 </tbody>
             </table>
