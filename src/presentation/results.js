449cd720b18c4989de10da78fd76101359a17de3
bahaa1985 <bahaa_salaheldin@live.com>
2023-02-09 22:38:55 +0200

    events problem

--- src/presentation/results.js
+++ src/presentation/results.js
@@ -4,10 +4,15 @@ import getResults from '../api/getResults'
 import  '../styles/results.css'
 import Events from './events'
 
+
+
 function Results(props){
     const [results,setResults]=useState([])
     const [fixture,setFixture]=useState(0)
     const [teams,setTeams]=useState([])
+    const [clicked,setClicked]=useState(false)
+    const [indexx,setIndex]=useState(0)
+    const fixtureRef=useRef(null)
     // const effect=useRef(false)
     const league=props.league
     const season=props.season
@@ -24,32 +29,40 @@ function Results(props){
         return group
     },{})
 
-    console.log('groupedResults: ',groupedResults)
+    // console.log('groupedResults: ',groupedResults)
     
+    useEffect(()=>{
+        const divv=document.getElementsByClassName('.fixture')[indexx]
+        // console.log('divv:',divv)
+    },[indexx])  
+
     return(
         <div>
             {               
-               Object.keys(groupedResults).sort((a,b)=>Date.parse(a)-Date.parse(b)).map((elem,index)=>{
+               Object.keys(groupedResults).sort((a,b)=>Date.parse(a)-Date.parse(b)).map((elem,day_index)=>{
                               
                 return(
                     <div>                
-                        <div key={index} className="fixture-date"> {elem} </div>                                     
-                        {groupedResults[elem].map((elem,index)=>{
+                        <div key={day_index} className="fixture-date"> {elem} </div>                                     
+                        {groupedResults[elem].map((elem,fixture_index)=>{
                             return(
                                 <div>
-                                    <div key={index} className="fixture" >
+                                    <div key={fixture_index} className="fixture" ref={fixtureRef} 
+                                        onClick={()=>{                                           
+                                            setIndex(index)
+                                        }}
+                                             >
                                         <img src={elem.teams.home.logo}></img>
                                         <span>{elem.teams.home.name}</span>
                                         <span>{elem.goals.home}</span>                                
                                         <span>{elem.goals.away}</span>
                                         <span>{elem.teams.away.name}</span>
                                         <img src={elem.teams.away.logo}></img>
-                                        <button  onClick={()=>{setFixture(elem.fixture.id);
-                                                 setTeams([elem.teams.home.id,elem.teams.away.id])}} >Details</button>
+                                        <button>{index}</button>
                                     </div> 
-                                    
+                                    {clicked ? <Events fixture={fixture} teams={teams}/> : null}
                                 </div>
-                            
+
                             )
                         })} 
                         
@@ -57,9 +70,19 @@ function Results(props){
                     )               
                 })
             }
-            <Events fixture={fixture} teams={teams}/>                                             
+                                                         
         </div>
     )
+
+    // append events jsx to fixture div after clicking details button:
+    function appendFixtureDetailsDiv(ref){
+    // const fixture_divs=document.getElementsByClassName('.fixture') 
+    console.log('fixture divs:',ref)
+    // fixture_divs.forEach(div => {
+        // div.removeChild(Events)
+    // });   
+    // fixture_divs[index].appendChild(Events)
+    }
 }
 
 export default Results
