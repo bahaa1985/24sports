ebb5cbb46058f57374257bada368c9574044997e
bahaa1985 <bahaa_salaheldin@live.com>
2023-03-20 23:08:30 +0200

    events 20-3

--- src/presentation/events.js
+++ src/presentation/events.js
@@ -12,7 +12,7 @@ function Events(props){
     const [events,setEvents]=useState([])
     const [homeMargin,setHomeMargin]=useState(0)
     const [awayMargin,setAwayMargin]=useState(0)
-    const events_div=useRef();
+    const event_div=useRef(HTMLElement);
     useEffect(()=>{
         getEvents(fixture).then((result)=>{
            setEvents( result.data.response )
@@ -28,72 +28,73 @@ function Events(props){
         return group;
     },[])
     console.log('grouped events: ',GROUPED_EVENTS)
-    return(
-      
-        <div className='events' ref={events_div}>
+
+    const home_events_div=(player,assist,type,index)=>{
+        return(
+            <div key={index} style={{display:'flex',justifyContent:'flex-start',margin:'5px auto'}}>
+            <span>{type}</span>
+            <div className="div-players">
+                <label className="label-palyer">{player}</label><br></br>
+                <label className="label-assist">{assist}</label>
+            </div>
+            </div>     )
+    }
+
+    const away_events_div=(player,assist,type,index)=>{
+        return(
+            <div key={index} style={{display:'flex',justifyContent:'flex-end',margin:'5px auto'}}>           
+            <div className="div-players">
+                <label className="label-palyer">{player}</label><br></br>
+                <label className="label-assist">{assist}</label>
+            </div>
+            <span>{type}</span>
+            </div>      )
+    }
+    return(        
+        <div className='events' >
             
             {
                 GROUPED_EVENTS.map((event,index)=>{                    
                     return(
-                        <Fragment>
-                            <div>{index}</div>
-                        </Fragment>
-                    )
-                })
-                    // if(event.length==1){
-                    //     return(
-                    //         <Fragment key={index}>
-                    //         {
-                    //            <div className="event">                                
-                    //             {event[0].team.id===HOME_TEAM?
-                    //                 <Fragment>
-                    //                     <div className="div-details" style={{justifyContent:'flex-end'}}>
-                    //                         <span className="span-type">{event[0].type}</span>
-                    //                         <div className="div-players" style={{textAlign:'right'}}>
-                    //                             <label className="label-palyer">{event[0].player.name}</label><br></br>
-                    //                             <label className="label-assist">{event[0].assist.name}</label>
-                    //                         </div>
-                    //                     </div>                                       
-                    //                     <span className="span-time">{index}</span>
-                    //                     <div className="div-empty">
-                    //                         <label className="span-empty"></label>                                            
-                    //                     </div>
-                                      
-                    //                 </Fragment>:null
+                       
+                            <div ref={event_div} style={{display:'flex',width:'60%'}}>
+                                {
+                                    <div>
+                                        {
+                                            eventsHome.map((event) => {                                        
+                                                return(
+                                                    event.time.elapsed===index? 
+                                                    home_events_div(event.player.name,event.assist.name,event.type,index):
+                                                    null
+                                                )                                                                                                                                
+                                            })
+                                        }
+                                    </div>
                                     
-                    //             }
-                               
-                    //             {event[0].team.id===AWAY_TEAM?
-                                                                       
-                    //                 <Fragment>
-                    //                     <div className="div-empty">
-                    //                         <label className="span-empty"></label>                                       
-                    //                     </div>
-                    //                     <span className="span-time">{index}</span>
-                    //                     <div className="div-details" style={{justifyContent:'flex-start'}}>
-                    //                         <div className="div-players" style={{textAlign:'left'}}>
-                    //                             <label className="label-player">{event[0].player.name}</label><br></br>
-                    //                             <label className="label-assist">{event[0].assist.name}</label>                                            
-                    //                         </div>                                     
-                    //                         <span className="span-type">{event[0].type}</span>
-                    //                     </div>                                       
-                                        
-                    //              </Fragment>:null
-                    //             }
-                    //            </div>
-                    //         }                            
-                    //     </Fragment> 
-                    //     )
-                    // }                                  
-            }
-            {
-                events_div.current.children.forEach(event_div => {
-                    eventsHome.map(homeEvent,index)
+                                }
+                                <span style={{width:'5%'}}>{index}</span>
+                                {
+                                    <div>
+                                        {
+                                            eventsAway.map((event) => {
+                                                                                
+                                                return(
+                                                    event.time.elapsed===index? 
+                                                    away_events_div(event.player.name,event.assist.name,event.type,index):
+                                                    null
+                                                )                                                                                        
+                                            
+                                            })
+                                        }
+                                    </div>
+                                    
+                                }  
+                            </div>
+                    )
                 })
             }
         </div>
-        
-    )
-}
+    )}
+                        
 
 export default Events
