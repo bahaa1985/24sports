4893060782a65d6716f2f1885e49ff330ce5fb5d
bahaa1985 <bahaa_salaheldin@live.com>
2023-04-17 18:27:53 +0200

    statistics problem 17-4

--- src/presentation/statistics.js
+++ src/presentation/statistics.js
@@ -7,8 +7,8 @@ function Statistics(props){
     const home_team=props.teams[0]
     const away_team=props.teams[1]
     const fixture=props.fixture
-    const [homeStatistics,setHomeStatistics]=useState(0)
-    const [awayStatistics,setAwayStatistics]=useState(0)
+    const [homeStatistics,setHomeStatistics]=useState([])
+    const [awayStatistics,setAwayStatistics]=useState([])
     
     let statistic_obj={
         home:'',
@@ -21,50 +21,60 @@ function Statistics(props){
     useEffect(()=>{
         getStatistics(fixture,home_team).then((result)=>{
             setHomeStatistics(result.data.response[0].statistics)
-        }); 
+        });                                      
+        console.log('home effect!')
+    },[])
+
+    useEffect(()=>{
         getStatistics(fixture,away_team).then((result)=>{
             setAwayStatistics(result.data.response[0].statistics)
-        });                
-             
-    },[fixture,home_team,away_team])
+        });
+        console.log('home effect!')
+    },[])
 
-    let home_arr=Array.from(homeStatistics)
-    let away_arr=Array.from(awayStatistics);    
+    // let home_arr=Array.from(homeStatistics)
+    // let away_arr=Array.from(awayStatistics);    
 
-    for(let i=0;i<16;i++){
-        statistic_obj={home:home_arr[i].value,
-                        type:home_arr[i].type,
-                        away:away_arr[i].value }
-        statistics_arr.push(statistic_obj)
-    }  
-
+   
     let total,max,factor=0;
     let screen_width=window.innerWidth
     let prgress_width=(45*screen_width)/100
     return(
         <div>           
-            {      
-            statistics_arr.map((element,index)=>{
-                total=element.home+element.away
-                max=prgress_width
-                factor=max/total                    
-                return(
-                <div>
-                    <div>{element.type}</div>
-                    <div className="statistics-details">
-                        <span>{element.home}</span>
-                        <div>
-                            <progress max={max} value={element.home*factor}></progress>
-                        </div>
-                        <div>
-                            <progress max={max} value={element.away*factor}></progress>
-                        </div>
-                        <span>{element.away}</span>
-                    </div>
-                </div>
-                )
-            })                          
-                
+            {
+                homeStatistics.map((index,value)=>{
+                    <span>{value.type}</span>
+                })
+                // ()=>{             
+                //     for(let i=0;i<homeStatistics.length;i++){
+                //        statistic_obj={home:homeStatistics[i].value,
+                //                        type:homeStatistics[i].type,
+                //                        away:awayStatistics[i].value }
+                //        statistics_arr.push(statistic_obj)
+                //    }  
+                    
+                //    statistics_arr.map((element,index)=>{
+                //        total=element.home+element.away
+                //        max=prgress_width
+                //        factor=max/total                    
+                //        return(
+                //        <div>
+                //            <div>{element.type}</div>
+                //            <div className="statistics-details">
+                //                <span>{element.home}</span>
+                //                <div>
+                //                    <progress max={max} value={element.home*factor}></progress>
+                //                </div>
+                //                <div>
+                //                    <progress max={max} value={element.away*factor}></progress>
+                //                </div>
+                //                <span>{element.away}</span>
+                //            </div>
+                //        </div>
+                //        )
+                //    })                          
+                       
+                //    }
             }
         </div>
     )
