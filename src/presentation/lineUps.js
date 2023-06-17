c3b49fcd57f8e5ee8a077ff76dae2773c77268d0
bahaa1985 <bahaa_salaheldin@live.com>
2023-06-17 05:47:35 +0300

    lineup v2 17-6

--- src/presentation/lineUps.js
+++ src/presentation/lineUps.js
@@ -10,40 +10,41 @@ function LineUps(props){
     const [homeTeam,setHomeTeam]=useState("");
     const [awayTeam,setAwayTeam]=useState("");
     const [homeLineUp,setHomeLineUp]=useState([]);
-    const [homeFormation,setHomeFormation]=useState("");
+    const [homeFormation,setHomeFormation]=useState([]);
     const [awayLineUp,setAwayLineUp]=useState([]);
-    const [awayFormation,setAwayFormation]=useState("");    
+    const [awayFormation,setAwayFormation]=useState([]);    
     let [clickedTeam,setClickedTeam]=useState("");
     let [grid,setGrid]=useState(0);
     
     useEffect(()=>{
         getLinesUps(fixtureId).then((result)=>{
             setHomeLineUp(result.data.response[0].startXI);
-            setHomeFormation(result.data.response[0].formation);
+            setHomeFormation(Array.from(result.data.response[0].formation.replaceAll('-','')));
             setHomeTeam(result.data.response[0].team.name);
             setAwayLineUp(result.data.response[1].startXI);
-            setAwayFormation(result.data.response[1].formation);
+            setAwayFormation(Array.from(result.data.response[1].formation.replaceAll('-','')));
             setAwayTeam(result.data.response[1].team.name);
         })
     },[fixtureId]);
 
-    console.log('Home start:',homeLineUp);
-    console.log('Away start:',awayLineUp);
-    let homeFormationArr= Array.from(homeFormation.replace('-',''));
-    let awayFormationArr= Array.from(awayFormation.replace('-',''));;
-    // let clickedTeam=''; 
-    let line_index=-1;
-    
-    function playerPosition(lineup,grid){       
-            lineup.filter((player)=>player.player.grid[0]===grid)
-                        .sort((playerA,playerB)=>parseInt(playerA.player.grid[2]) - parseInt(playerB.player.grid[2]))
-                        .map((player,index)=>{
-                            return(
-                                <div key={index}>
-                                    {player.player.name}
-                                </div>
-                            )
-                        })        
+    // let homeFormationArr= [];
+    // let awayFormationArr= [];
+
+    // console.log('Home start:',homeLineUp);
+    // console.log('Away start:',awayLineUp);
+               
+    function playerPosition(lineup,grid){
+            console.log('this is line up:',lineup)       
+            // lineup.filter((player)=>player.player.grid[0]===grid)
+            //             .sort((playerA,playerB)=>parseInt(playerA.player.grid[2]) - parseInt(playerB.player.grid[2]))
+            //             .map((player,index)=>{                            
+            //                     <div key={index}>
+            //                         {player.player.name}
+            //                     </div>                            
+            //             }) 
+            lineup.forEach(element => {
+                return(<div>{element}</div>)
+            });       
     }
     
     return(    
@@ -54,41 +55,29 @@ function LineUps(props){
             </div>
             <div className='pitch'>
                 {                    
-                    clickedTeam==="h"?                                                                                                                                
-                            
-                            (
-                              <div>
-                                    hi
+                    clickedTeam==="h"?                                                                                                                                                                                                                   
+                        [                           
+                            <div>hi home</div>, 
+                            homeFormation.map((item,index)=>{                                                        
+                                <div className='line' key={index}>
+                                {
+                                    playerPosition(homeLineUp,(index+2).toString())                                                                           
+                                }
                                 </div>
-                            )
-                            (
-                                setGrid(1),
-                                homeFormationArr.map((item,index)=>{                                   
-                                    setGrid(grid++),
-                                   <div className='line'>
-                                    {playerPosition(homeLineUp,grid.toString())}
-                                    </div>
-                                })
-                            )                                                   
-                       
+                            })
+                        ]                                                   
                     :
 
                     clickedTeam==="a"? 
                                           
-                        (
-                          <div>
-                               hi
-                            </div>
-                        )
-                        (
-                            setGrid(1),
-                            awayFormationArr.map((item,index)=>{                                
-                                setGrid(grid++);
+                        [                       
+                          <div>hi away</div>,                        
+                            awayFormation.map((item,index)=>{                                                
                                 <div className='line'>
-                                {playerPosition(awayLineUp,grid.toString())}
+                                {playerPosition(awayLineUp,(index+2).toString())}                    
                                 </div>
                             })
-                        )
+                        ]
                        
                     :null                    
                 }
