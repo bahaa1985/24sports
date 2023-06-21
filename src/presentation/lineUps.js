84d74936c78461703ea1eac230206e86a2e48927
bahaa1985 <bahaa_salaheldin@live.com>
2023-06-22 02:33:56 +0300

    line up 21-6 v3

--- src/presentation/lineUps.js
+++ src/presentation/lineUps.js
@@ -14,7 +14,7 @@ function PlayerPosition(props){
             
             let style_width='';
             
-            if(sp_lineup.length===3){
+            if(sp_lineup.length<=2){
                 style_width='25%'
             }
             else if(sp_lineup.length===3){
@@ -32,15 +32,14 @@ function PlayerPosition(props){
                     {                    
                         sp_lineup.map((player,index)=>{
                             return(
-                                <div key={index} className='player-card' style={{width:style_width}}>
+                                <div key={index} className='player-card' >
                                     <div className="player-rating">{player.statistics[0].games.rating}</div>
                                    
                                     <img className='player-photo' src={player.player.photo}></img>
                                     
-                                    <div className='player-name'>{player.player.name}</div>
-                                   
                                     <div className='player-number'>{player.player.number}</div>
-                                                                 
+
+                                    <div className='player-name'>{player.player.name}</div>                                                                                                                                        
                                 </div>
                             )
                         })                       
@@ -117,38 +116,37 @@ function LineUps(props){
                     <>
                         <div>Formation: {homeFormation.join('-')}</div> 
                         <div className='pitch'>
-                                <div className='goalkeeper' key={1}>
-                                    <div>
+                            <div className='line' key={1} >   
+                                <div className='player-card' style={{width:'25%'}} key={1}>
+                                   
                                         <div className='player-rating'>{homeLineUp[0].statistics[0].games.rating}</div>
                                         
                                         <img src={homeLineUp[0].player.photo} className='player-photo'></img>
-                                       
-                                        <div className='palyer-name'>{homeLineUp[0].player.name}</div>
-                                       
-                                        <div className='palyer-number'>{homeLineUp[0].player.number}</div>
+                                                                                                                    
+                                        <div className='player-number'>{homeLineUp[0].player.number}</div>
                                         
-                                        
-                                    </div>
-                                    
-                                </div>
+                                        <div className='player-name'>{homeLineUp[0].player.name}</div>
                                 
-                                <div className='line' key={2} >                                    
-                                    <PlayerPosition lineup={homeLineUp} grid={"2"} />                                    
                                 </div>
-                                
-                                <div className='line' key={3} >                                    
-                                    <PlayerPosition lineup={homeLineUp} grid={"3"} />                                   
-                                </div>
-                                
-                                <div className='line' key={4}>                                    
-                                    <PlayerPosition lineup={homeLineUp} grid={"4"} />                                   
-                                </div>
-                                {
-                                    homeFormation.length>3 ?
-                                    <div className='line' key={5} >                                   
-                                        <PlayerPosition lineup={homeLineUp} grid={"5"} />                                   
-                                    </div>:null
-                                }                            
+                            </div>
+                            
+                            <div className='line' key={2} >                                    
+                                <PlayerPosition lineup={homeLineUp} grid={"2"} />                                    
+                            </div>
+                            
+                            <div className='line' key={3} >                                    
+                                <PlayerPosition lineup={homeLineUp} grid={"3"} />                                   
+                            </div>
+                            
+                            <div className='line' key={4}>                                    
+                                <PlayerPosition lineup={homeLineUp} grid={"4"} />                                   
+                            </div>
+                            {
+                                homeFormation.length>3 ?
+                                <div className='line' key={5} >                                   
+                                    <PlayerPosition lineup={homeLineUp} grid={"5"} />                                   
+                                </div>:null
+                            }                            
                         </div>
                     </>
                                                                     
@@ -158,14 +156,16 @@ function LineUps(props){
                 <>
                     <div>Formation: {awayFormation.join('-')}</div> 
                     <div className='pitch'> 
-                        <div className='goalkeeper' key={1} >
-                            <span>{awayLineUp[0].statistics[0].games.rating}</span>
-                            <br/>
-                            <img src={awayLineUp[0].player.photo} className='player-photo'></img>
-                            <br/>
-                            <span>{awayLineUp[0].player.name}</span>
-                            <br></br>
-                            <span>{awayLineUp[0].player.number}</span>                           
+                        <div className='line' key={1} > 
+                            <div className='player-card' style={{width:'25%'}} key={1} >
+                                <div className='player-rating'>{awayLineUp[0].statistics[0].games.rating}</div>                            
+                                
+                                <img src={awayLineUp[0].player.photo} className='player-photo'></img>                            
+                                                        
+                                <div className='player-number'>{awayLineUp[0].player.number}</div>                           
+
+                                <div className='player-name'>{awayLineUp[0].player.name}</div>
+                            </div>
                         </div>
                         
                         <div className='line' key={2}>                                    
